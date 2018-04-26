76c19717f2d3f66b0635cf39adebf94800e3c0ab
John Kelvie <john.kelvie@daimler.com>
2018-04-26 00:23:18 -0500

    Added assertion tests

--- test/Assertion.test.js
+++ test/Assertion.test.js
@@ -1,10 +1,39 @@
 const Assertion = require("../lib/Assertion");
 
 describe("assertion", () => {
-    test("evaluate", () => {
+    test("evaluate == a string", () => {
         const obj = { val: "Here is a test" };
-        const assertion = new Assertion("val", "==", "\"Here is a test\"");
+        let assertion = new Assertion("val", "==", "Here is a test");
         expect(assertion.evaluate(obj)).toBe(true);
 
+        assertion = new Assertion("val", "==", "\"Here is a test\"");
+        expect(assertion.evaluate(obj)).toBe(true);
+
+        assertion = new Assertion("val", "==", "Here it will fail");
+        expect(assertion.evaluate(obj)).toBe(false);
+    });
+
+    test("evaluate == undefined", () => {
+        const obj = { val: undefined, val2: "a" };
+        let assertion = new Assertion("val", "==", "undefined");
+        expect(assertion.evaluate(obj)).toBe(true);
+
+        assertion = new Assertion("val", "==", undefined);
+        expect(assertion.evaluate(obj)).toBe(true);
+
+        assertion = new Assertion("val2", "==", undefined);
+        expect(assertion.evaluate(obj)).toBe(false);
+    });
+
+    test("evaluate != undefined", () => {
+        const obj = { val: undefined, val2: "a" };
+        let assertion = new Assertion("val2", "!=", "undefined");
+        expect(assertion.evaluate(obj)).toBe(true);
+
+        assertion = new Assertion("val2", "!=", undefined);
+        expect(assertion.evaluate(obj)).toBe(true);
+
+        assertion = new Assertion("val", "!=", undefined);
+        expect(assertion.evaluate(obj)).toBe(false);
     });
 });
