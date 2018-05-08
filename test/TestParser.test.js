47bc0832bc7463af313817739447dab842241023
John Kelvie <john.kelvie@gmail.com>
2018-05-08 15:46:20 -0500

    Added support for assertion objects

--- test/TestParser.test.js
+++ test/TestParser.test.js
@@ -21,12 +21,12 @@ describe("test parser", () => {
         expect(secondTest.interactions[0].utterance).toEqual("Get New Facts");
         expect(secondTest.interactions[0].assertions.length).toEqual(3);
 
-        const firstAssertion = secondTest.interactions[0].assertions[0];
+        const firstAssertion = secondTest.interactions[0].assertions[1];
         expect(firstAssertion.path).toEqual("response.outputSpeech.ssml");
         expect(firstAssertion.operator).toEqual("==");
         expect(firstAssertion.value).toEqual("Here's your fact:*");
 
-        const secondAssertion = secondTest.interactions[0].assertions[1];
+        const secondAssertion = secondTest.interactions[0].assertions[2];
         expect(secondAssertion.path).toEqual("response.card.title");
         expect(secondAssertion.operator).toEqual("==");
         expect(secondAssertion.value).toEqual("Space Facts");
@@ -42,5 +42,31 @@ describe("test parser", () => {
         expect(() => {
             parser.parse();
         }).toThrowError("Invalid operator: ===");
+    });
+
+    test("parses simple test file with object assertions", () => {
+        const parser = new TestParser();
+        parser.load(`
+--- 
+- LaunchRequest: # LaunchRequest is "reserved" - it is not an utterance but a request type
+  - response.outputSpeech.ssml: "Here's your fact: *"  
+  - response.outputSpeech.ssml: /regular expression+.*/  
+  - response.outputSpeech.ssml: "/Here's your fact: .*/"  
+        `)
+        const testSuite = parser.parse();
+        const assertion = testSuite.tests[0].interactions[0].assertions[0];
+        expect(assertion.path).toEqual("response.outputSpeech.ssml");
+        expect(assertion.operator).toEqual("==");
+        expect(assertion.value).toEqual("Here's your fact: *");
+
+        const assertion2 = testSuite.tests[0].interactions[0].assertions[1];
+        expect(assertion2.path).toEqual("response.outputSpeech.ssml");
+        expect(assertion2.operator).toEqual("=~");
+        expect(assertion2.value).toEqual("/regular expression+.*/");
+
+        const assertion3 = testSuite.tests[0].interactions[0].assertions[2];
+        expect(assertion3.path).toEqual("response.outputSpeech.ssml");
+        expect(assertion3.operator).toEqual("=~");
+        expect(assertion3.value).toEqual("/Here's your fact: .*/");
     });
 });
