6571416cbe7fa01618254a7263ed7bdd56a1de4b
John Kelvie <john.kelvie@gmail.com>
2018-05-10 20:07:54 -0500

    Added tests for JestAdapter
    Upgraded cosmiconfig
    Fixed bug in Test metadata parsing

---
+++ test/JestAdapter.test.js
@@ -0,0 +1,68 @@
+const Assertion = require("../lib/Assertion");
+const InteractionResult = require("../lib/TestResult").InteractionResult;
+const Test = require("../lib/Test");
+const TestInteraction = require("../lib/TestInteraction");
+const TestResult = require("../lib/TestResult").TestResult;
+const testRunner = require("../lib/JestAdapter");
+
+describe("JestAdapter", async () => {
+    test("Runs a mock test that succeeds", async () => {
+        const test = new Test({ description: "Test Description" });
+        const testResult = new TestResult(test);
+        const interaction = new TestInteraction("Hi");
+        const interactionResult = new InteractionResult(interaction)
+        testResult.addInteractionResult(interactionResult);
+        const results = [testResult];
+
+        const jestResults = await testRunner({}, {}, {}, new Runtime(results), "MyTest.yml");
+        expect(jestResults.numPassingTests).toBe(1);
+        expect(jestResults.numFailingTests).toBe(0);
+        expect(jestResults.testResults.length).toBe(1);
+        expect(jestResults.testFilePath).toBe("MyTest.yml");
+
+        // Check the individual test result
+        const jestTestResult = jestResults.testResults[0]
+        expect(jestTestResult.ancestorTitles[0]).toBe("Test Description");
+        expect(jestTestResult.status).toBe("passed");
+    });
+
+    test("Runs a mock test that fails", async () => {
+        const test = new Test({ description: "Test Description" });
+        const testResult = new TestResult(test);
+        const interaction = new TestInteraction("Hi");
+        const assertion = new Assertion("path", "==", "value");
+        const interactionResult = new InteractionResult(interaction, assertion, "Here is an error");
+        testResult.addInteractionResult(interactionResult);
+        const results = [testResult];
+
+        const jestResults = await testRunner({}, { rootDir: "rootDir" }, {}, new Runtime(results), "MyTest.yml");
+        expect(jestResults.numPassingTests).toBe(0);
+        expect(jestResults.numFailingTests).toBe(1);
+        expect(jestResults.testResults.length).toBe(1);
+
+        // Check the magical failure message
+        expect(jestResults.failureMessage).toContain("Test Description › Hi");
+        expect(jestResults.failureMessage).toContain("Here is an error");
+
+        // Check the individual test result
+        const jestTestResult = jestResults.testResults[0]
+        expect(jestTestResult.ancestorTitles[0]).toBe("Test Description");
+        expect(jestTestResult.status).toBe("failed");
+    });
+});
+
+class Runtime {
+    constructor(results) {
+        FakeVirtualAlexaRunner.results = results;
+    }
+
+    requireModule () {
+        return FakeVirtualAlexaRunner;
+    }
+}
+
+class FakeVirtualAlexaRunner {
+    run() {
+        return FakeVirtualAlexaRunner.results;
+    }
+}
