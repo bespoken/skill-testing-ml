c4e6e6bd760636fa17f32cba3038bc166043e408
Edgar Cruzado <edgar@bespoken.io>
2018-06-27 07:39:32 -0500

    Rename method, emit errors, add unit test

--- test/TestRunner.test.js
+++ test/TestRunner.test.js
@@ -2,24 +2,22 @@ const Configuration = require("../lib/runner/Configuration");
 const TestRunner = require("../lib/runner/TestRunner");
 
 describe("test runner", () => {
-    beforeEach(() => {
-        return Configuration.configure({
-            handler: "test/FactSkill/index.handler",
-            interactionModel: "test/FactSkill/models/en-US.json",
-            locale: "en-US"
-        });
-    });
-
     afterEach(() => {
         Configuration.singleton = undefined;
     });
 
     test("subscribe()", async () => {
-        const runner = new TestRunner();
+        const runner = new TestRunner({
+            handler: "test/FactSkill/index.handler",
+            interactionModel: "test/FactSkill/models/en-US.json",
+            locale: "en-US"
+        });
         const messageCallback = (error, test) => {
+            expect(error).toBeUndefined();
             expect(test.description).toBeDefined();
         };
         const resultCallback = (error, test) => {
+            expect(error).toBeUndefined();
             expect(test.interactionResults).toBeDefined();
         };
         const messageCallbackMock = jest.fn(messageCallback);
@@ -27,9 +25,57 @@ describe("test runner", () => {
         runner.subscribe("message", messageCallbackMock);
         runner.subscribe("result", resultCallbackMock);
   
-        await runner.run("test/FactSkill/fact-skill-tests.common.yml");
+        await runner.run("test/FactSkill/fact-skill-tests.yml");
         
         expect(messageCallbackMock).toHaveBeenCalledTimes(3);
         expect(resultCallbackMock).toHaveBeenCalledTimes(3);
+    });
+
+    test("unsubscribe()", async () => {
+        const runner = new TestRunner({
+            handler: "test/FactSkill/index.handler",
+            interactionModel: "test/FactSkill/models/en-US.json",
+            locale: "en-US"
+        });
+        const messageCallback = (error, test) => {
+            expect(error).toBeUndefined();
+            expect(test.description).toBeDefined();
+        };
+        const resultCallback = (error, test) => {
+            expect(error).toBeUndefined();
+            expect(test.interactionResults).toBeDefined();
+        };
+        const messageCallbackMock = jest.fn(messageCallback);
+        const resultCallbackMock = jest.fn(resultCallback);
+        runner.subscribe("message", messageCallbackMock);
+        runner.subscribe("result", resultCallbackMock);
+        runner.unsubscribe("message");
+        runner.unsubscribe("result");
+
+  
+        await runner.run("test/FactSkill/fact-skill-tests.yml");
+        
+        expect(messageCallbackMock).toHaveBeenCalledTimes(0);
+        expect(resultCallbackMock).toHaveBeenCalledTimes(0);
+    });
+
+    test("error", async () => {
+        const runner = new TestRunner({
+            invoker: "VirtualDeviceInvoker",
+        });
+        const messageCallback = (error) => {
+            expect(error).toBeDefined();
+        };
+        const resultCallback = (error) => {
+            expect(error).toBeDefined();
+        };
+        const messageCallbackMock = jest.fn(messageCallback);
+        const resultCallbackMock = jest.fn(resultCallback);
+        runner.subscribe("message", messageCallbackMock);
+        runner.subscribe("result", resultCallbackMock);
+  
+        await runner.run("test/FactSkill/fact-skill-tests.yml");
+        
+        expect(resultCallbackMock).toHaveBeenCalledTimes(1);
     });
 });
