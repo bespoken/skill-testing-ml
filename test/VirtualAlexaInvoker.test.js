651353c71ae7e847d33b14e5a3911d46d6646a8c
Edgar Cruzado <edgar@bespoken.io>
2018-07-05 13:17:14 -0500

    Add support for succinct intent syntax

--- test/VirtualAlexaInvoker.test.js
+++ test/VirtualAlexaInvoker.test.js
@@ -1,6 +1,7 @@
 const Configuration = require("../lib/runner/Configuration");
 const TestRunner = require("../lib/runner/TestRunner");
 const TestSuite = require("../lib/test/TestSuite");
+const VirtualAlexaInvoker = require("../lib/runner/VirtualAlexaInvoker");
 
 describe("virtual alexa runner", () => {
     describe("basic tests", () => {
@@ -366,6 +367,49 @@ describe("virtual alexa runner", () => {
             const runner = new TestRunner();
             const results = await runner.run("test/PetMatchSkill/multiLocale.externalized.yml");                    
             expect(results.length).toEqual(4);
+        });
+    });
+
+    describe("succinct intent", () => {
+        test("get intent", async () => {
+            const invoker = new VirtualAlexaInvoker();
+
+            let intent = invoker.detectIntent("PetMatchIntent size=mini");
+            expect(intent.name).toBe("PetMatchIntent");
+            expect(intent.slots.length).toBe(1);
+            expect(intent.slots[0]).toEqual({size: "mini"});
+
+            intent = invoker.detectIntent("PetMatchIntent size=mini temperament=guard");
+            expect(intent.name).toBe("PetMatchIntent");
+            expect(intent.slots.length).toBe(2);
+            expect(intent.slots[0]).toEqual({size: "mini"});
+            expect(intent.slots[1]).toEqual({temperament: "guard"});
+
+            intent = invoker.detectIntent("PetMatchIntent size=mini temperament=guard energy=low");
+            expect(intent.name).toBe("PetMatchIntent");
+            expect(intent.slots.length).toBe(3);
+            expect(intent.slots[0]).toEqual({size: "mini"});
+            expect(intent.slots[1]).toEqual({temperament: "guard"});
+            expect(intent.slots[2]).toEqual({energy: "low"});
+
+            intent = invoker.detectIntent("PetMatchIntent size=\"mini\" temperament=guard");
+            expect(intent.name).toBe("PetMatchIntent");
+            expect(intent.slots.length).toBe(2);
+            expect(intent.slots[0]).toEqual({size: "mini"});
+            expect(intent.slots[1]).toEqual({temperament: "guard"});
+
+            intent = invoker.detectIntent("PetMatchIntent size=\"mini mini\" temperament=guard");
+            expect(intent.name).toBe("PetMatchIntent");
+            expect(intent.slots.length).toBe(2);
+            expect(intent.slots[0]).toEqual({size: "mini mini"});
+            expect(intent.slots[1]).toEqual({temperament: "guard"});
+            
+            intent = invoker.detectIntent("PetMatchIntent size=\"mini mini\" temperament=\"guard guard\"");
+            expect(intent.name).toBe("PetMatchIntent");
+            expect(intent.slots.length).toBe(2);
+            expect(intent.slots[0]).toEqual({size: "mini mini"});
+            expect(intent.slots[1]).toEqual({temperament: "guard guard"});
+
         });
     });
 });
