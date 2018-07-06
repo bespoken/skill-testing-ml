163d2f92b3dcd0b8c4fb99e3e3d248cc166e625e
Juan Perata <juan@bespoken.tools>
2018-07-06 04:23:05 -0500

    Integrate virtual google assistant into skill tester

---
+++ test/VirtualGoogleAssistantInvoker.test.js
@@ -0,0 +1,29 @@
+const Configuration = require("../lib/runner/Configuration");
+const TestRunner = require("../lib/runner/TestRunner");
+
+describe("virtual google assistant runner", () => {
+    describe("basic tests", () => {
+        beforeEach(() => {
+            Configuration.configure({
+                directory: "test/SillyNameMakerExpress/dialogFlow",
+                expressModule: "test/SillyNameMakerExpress/index",
+                expressPort: 3000,
+                invoker: "VirtualGoogleAssistantInvoker",
+                locale: "en-US"
+            });
+        });
+
+        afterEach(() => {
+            Configuration.singleton = undefined;
+        });
+
+        test("runs fact skill test", async () => {
+            const runner = new TestRunner();
+
+            const results = await runner.run("test/SillyNameMakerExpress/silly-name-maker-tests.yml");
+            expect(results.length).toEqual(1);
+            expect(results[0].test.description).toEqual("Launches successfully");
+            expect(results[0].interactionResults[0].error).toBeUndefined();
+        });
+    });
+});
