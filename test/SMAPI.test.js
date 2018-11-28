e56ec72607e3ec19120c3ce07a681f220662088e
John Kelvie <jpk@bespoken.io>
2018-11-27 19:01:20 -0500

    Another trick for nock to stop

--- test/SMAPI.test.js
+++ test/SMAPI.test.js
@@ -13,7 +13,7 @@ const describeIf = process.env.ASK_ACCESS_TOKEN ? describe : describe.skip;
 describeIf("test suite", () => {
     jest.setTimeout(30000);
     beforeAll(() => {
-        delete require.cache[require.resolve("nock")];
+        delete require.cache[require.resolve("https")];
         
         // Create an ask config if it does not exist
         const askConfigPath = path.join(os.homedir(), ".ask/cli_config");
