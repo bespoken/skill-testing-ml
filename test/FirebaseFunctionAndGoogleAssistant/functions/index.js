60faab281d387950b0dad9eb890ae6f5c0770f2e
Juan Perata <juan@bespoken.tools>
2018-12-13 18:23:10 -0500

    Google Action update and tests

---
+++ test/FirebaseFunctionAndGoogleAssistant/functions/index.js
@@ -0,0 +1,11 @@
+const {dialogflow, BasicCard} = require("actions-on-google");
+const functions = require("firebase-functions")
+
+const app = dialogflow( {debug: true} );
+
+app.intent("Default Welcome Intent", (conv) => {
+    const speechText = "This is the sample welcome! What would you like?";
+    conv.ask(speechText);
+});
+
+exports.myFunction = functions.https.onRequest(app);
