2e47c7f3f50801025465d5a075c09c097afb2888
Juan Perata <juan@bespoken.tools>
2018-12-14 11:44:12 -0500

    Remove debug from internal test project

--- test/FirebaseFunctionAndGoogleAssistant/functions/index.js
+++ test/FirebaseFunctionAndGoogleAssistant/functions/index.js
@@ -1,7 +1,7 @@
 const {dialogflow, BasicCard} = require("actions-on-google");
 const functions = require("firebase-functions")
 
-const app = dialogflow( {debug: true} );
+const app = dialogflow();
 
 app.intent("Default Welcome Intent", (conv) => {
     const speechText = "This is the sample welcome! What would you like?";
