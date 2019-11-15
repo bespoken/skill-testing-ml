371e1442c06a9fab782e195af1d43bac5b45aabc
Juan Perata <juan@bespoken.tools>
2019-11-15 04:05:22 -0500

    Change assertion evaluate to receive localizedValues

--- test/Assertion.test.js
+++ test/Assertion.test.js
@@ -5,13 +5,13 @@ describe("assertion", () => {
     test("evaluate == a string", () => {
         const obj = new MockResponse({ val: "Here is a test" });
         let assertion = new Assertion(undefined, "val", "==", "Here is a test");
-        expect(assertion.evaluate(obj)).toBe(true);
+        expect(assertion.evaluate(obj, assertion.value)).toBe(true);
 
         assertion = new Assertion(undefined, "val", "==", "\"Here is a test\"");
-        expect(assertion.evaluate(obj)).toBe(true);
+        expect(assertion.evaluate(obj, assertion.value)).toBe(true);
 
         assertion = new Assertion(undefined, "val", "==", "Here it will fail");
-        expect(assertion.evaluate(obj)).toBe(false);
+        expect(assertion.evaluate(obj, assertion.value)).toBe(false);
 
         const assertionString = assertion.toString(obj);
         expect(assertionString).toContain("Expected value at [val] to ==\n");
@@ -21,52 +21,52 @@ describe("assertion", () => {
     test("evaluate regex", () => {
         let obj = new MockResponse({ val: "Here is a test" });
         let assertion = new Assertion(undefined, "val", "=~", "/.*/");
-        expect(assertion.evaluate(obj)).toBe(true);
+        expect(assertion.evaluate(obj, assertion.value)).toBe(true);
 
         assertion = new Assertion(undefined, "val", "=~", "/here is a test/i");
-        expect(assertion.evaluate(obj)).toBe(true);
+        expect(assertion.evaluate(obj, assertion.value)).toBe(true);
 
         assertion = new Assertion(undefined, "val", "=~", "/here is a test/");
-        expect(assertion.evaluate(obj)).toBe(false);
+        expect(assertion.evaluate(obj, assertion.value)).toBe(false);
 
         assertion = new Assertion(undefined, "val", "=~", ".*");
-        expect(assertion.evaluate(obj)).toBe(true);
+        expect(assertion.evaluate(obj, assertion.value)).toBe(true);
 
         obj = new MockResponse({ val: "1980" });
         assertion = new Assertion(undefined, "val", "=~", "\\d+");
-        expect(assertion.evaluate(obj)).toBe(true);
+        expect(assertion.evaluate(obj, assertion.value)).toBe(true);
 
         obj = new MockResponse({ val: "word" });
         assertion = new Assertion(undefined, "val", "=~", "\\w+");
-        expect(assertion.evaluate(obj)).toBe(true);
+        expect(assertion.evaluate(obj, assertion.value)).toBe(true);
 
         obj = new MockResponse({ val: "welcome to guess" });
         assertion = new Assertion(undefined, "val", "=~", "\\d+");
-        expect(assertion.evaluate(obj)).toBe(false);
+        expect(assertion.evaluate(obj, assertion.value)).toBe(false);
     });
 
     test("evaluate array", () => {
         const obj = new MockResponse({ val: "Here is a test +" });
         let assertion = new Assertion(undefined, "val", "=~", ["/.*/", "not a test"]);
-        expect(assertion.evaluate(obj)).toBe(true);
+        expect(assertion.evaluate(obj, assertion.value)).toBe(true);
 
         assertion = new Assertion(undefined, "val", "==", ["/Not here.*/", "Here is a test +"]);
-        expect(assertion.evaluate(obj)).toBe(true);
+        expect(assertion.evaluate(obj, assertion.value)).toBe(true);
 
         assertion = new Assertion(undefined, "val", "==", ["Here is", "Here does not match"]);
-        expect(assertion.evaluate(obj)).toBe(true);
+        expect(assertion.evaluate(obj, assertion.value)).toBe(true);
 
         assertion = new Assertion(undefined, "val", "==", ["Not match", "Here does not match"]);
-        expect(assertion.evaluate(obj)).toBe(false);
+        expect(assertion.evaluate(obj, assertion.value)).toBe(false);
         let assertionString = assertion.toString(obj);
         expect(assertionString).toContain("Expected value at [val] to be one of:\n");
         expect(assertionString).toContain("\tNot match\n\tHere does not match\n");
 
         assertion = new Assertion(undefined, "val", "!=", ["not match", "not match 2"]);
-        expect(assertion.evaluate(obj)).toBe(true);
+        expect(assertion.evaluate(obj, assertion.value)).toBe(true);
 
         assertion = new Assertion(undefined, "val", "!=", ["Here is", "Here does not match"]);
-        expect(assertion.evaluate(obj)).toBe(false);
+        expect(assertion.evaluate(obj, assertion.value)).toBe(false);
         assertionString = assertion.toString(obj);
         expect(assertionString).toContain("Expected value at [val] to be one of:\n");
         expect(assertionString).toContain("\tHere is\n\tHere does not match\n");        
@@ -81,38 +81,38 @@ describe("assertion", () => {
 
         // Core operator tests
         let assertion = new Assertion(undefined, "number", ">", 99);
-        expect(assertion.evaluate(obj)).toBe(true);
+        expect(assertion.evaluate(obj, assertion.value)).toBe(true);
 
         assertion = new Assertion(undefined, "number", ">", 100);
-        expect(assertion.evaluate(obj)).toBe(false);
+        expect(assertion.evaluate(obj, assertion.value)).toBe(false);
 
         assertion = new Assertion(undefined, "number", ">=", 100);
-        expect(assertion.evaluate(obj)).toBe(true);
+        expect(assertion.evaluate(obj, assertion.value)).toBe(true);
 
         assertion = new Assertion(undefined, "number", ">=", 101);
-        expect(assertion.evaluate(obj)).toBe(false);
+        expect(assertion.evaluate(obj, assertion.value)).toBe(false);
 
         assertion = new Assertion(undefined, "number", "<=", 100);
-        expect(assertion.evaluate(obj)).toBe(true);
+        expect(assertion.evaluate(obj, assertion.value)).toBe(true);
 
         assertion = new Assertion(undefined, "number", "<=", 99);
-        expect(assertion.evaluate(obj)).toBe(false);
+        expect(assertion.evaluate(obj, assertion.value)).toBe(false);
 
         assertion = new Assertion(undefined, "number", "<", 101);
-        expect(assertion.evaluate(obj)).toBe(true);
+        expect(assertion.evaluate(obj, assertion.value)).toBe(true);
 
         assertion = new Assertion(undefined, "number", "<", 100);
-        expect(assertion.evaluate(obj)).toBe(false);
+        expect(assertion.evaluate(obj, assertion.value)).toBe(false);
 
         // Check some edge cases
         assertion = new Assertion(undefined, "number", ">", "50");
-        expect(assertion.evaluate(obj)).toBe(true);
+        expect(assertion.evaluate(obj, assertion.value)).toBe(true);
 
         assertion = new Assertion(undefined, "numberString", ">", "50");
-        expect(assertion.evaluate(obj)).toBe(true);
+        expect(assertion.evaluate(obj, assertion.value)).toBe(true);
 
         assertion = new Assertion(undefined, "notNumber", ">", "50");
-        expect(assertion.evaluate(obj)).toBe(false);
+        expect(assertion.evaluate(obj, assertion.value)).toBe(false);
         const assertionString = assertion.toString(obj);
         expect(assertionString).toContain("Expected value at [notNumber] to be >\n");
     });
@@ -120,77 +120,77 @@ describe("assertion", () => {
     test("evaluate wild cards", () => {
         const obj = new MockResponse({ val: "Here $ is ^ a + test?" });
         let assertion = new Assertion(undefined, "val", "==", "Here $ is ^ a + *");
-        expect(assertion.evaluate(obj)).toBe(true);
+        expect(assertion.evaluate(obj, assertion.value)).toBe(true);
 
         assertion = new Assertion(undefined, "val", "==", "Here is");
-        expect(assertion.evaluate(obj)).toBe(false);
+        expect(assertion.evaluate(obj, assertion.value)).toBe(false);
 
         assertion = new Assertion(undefined, "val", "==", "Here $ is *?");
-        expect(assertion.evaluate(obj)).toBe(true);
+        expect(assertion.evaluate(obj, assertion.value)).toBe(true);
     });
 
     test("evaluate == undefined", () => {
         const obj = new MockResponse({ val: undefined, val2: "a" });
         let assertion = new Assertion(undefined, "val", "==", "undefined");
-        expect(assertion.evaluate(obj)).toBe(true);
+        expect(assertion.evaluate(obj, assertion.value)).toBe(true);
 
         assertion = new Assertion(undefined, "val", "==", undefined);
-        expect(assertion.evaluate(obj)).toBe(true);
+        expect(assertion.evaluate(obj, assertion.value)).toBe(true);
 
         assertion = new Assertion(undefined, "val2", "==", undefined);
-        expect(assertion.evaluate(obj)).toBe(false);
+        expect(assertion.evaluate(obj, assertion.value)).toBe(false);
     });
 
     test("evaluate != undefined", () => {
         const obj = new MockResponse({ val: undefined, val2: "a" });
         let assertion = new Assertion(undefined, "val2", "!=", "undefined");
-        expect(assertion.evaluate(obj)).toBe(true);
+        expect(assertion.evaluate(obj, assertion.value)).toBe(true);
 
         assertion = new Assertion(undefined, "val3", "!=", "z");
-        expect(assertion.evaluate(obj)).toBe(true);
+        expect(assertion.evaluate(obj, assertion.value)).toBe(true);
 
         assertion = new Assertion(undefined, "val2", "!=", undefined);
-        expect(assertion.evaluate(obj)).toBe(true);
+        expect(assertion.evaluate(obj, assertion.value)).toBe(true);
 
         assertion = new Assertion(undefined, "val", "!=", undefined);
-        expect(assertion.evaluate(obj)).toBe(false);
+        expect(assertion.evaluate(obj, assertion.value)).toBe(false);
 
         assertion = new Assertion(undefined, "val2", "!=", ["x", "y", "z"]);
-        expect(assertion.evaluate(obj)).toBe(true);
+        expect(assertion.evaluate(obj, assertion.value)).toBe(true);
 
         assertion = new Assertion(undefined, "val3", "!=", ["x", "y", "z"]);
-        expect(assertion.evaluate(obj)).toBe(true);
+        expect(assertion.evaluate(obj, assertion.value)).toBe(true);
 
         assertion = new Assertion(undefined, "val2", "!=", undefined);
-        expect(assertion.evaluate(obj)).toBe(true);
+        expect(assertion.evaluate(obj, assertion.value)).toBe(true);
     });
 
     test("ignore case on string assertions", () => {
         const obj = new MockResponse({ ignoreCase: "TeSt2" });
         let assertion = new Assertion(undefined, "ignoreCase", "==", "test2");
-        expect(assertion.evaluate(obj)).toBe(true);
+        expect(assertion.evaluate(obj, assertion.value)).toBe(true);
 
         assertion = new Assertion(undefined, "ignoreCase", "==", "TEST2");
-        expect(assertion.evaluate(obj)).toBe(true);
+        expect(assertion.evaluate(obj, assertion.value)).toBe(true);
 
         assertion = new Assertion(undefined, "ignoreCase", "=~", "/TEST2/");
-        expect(assertion.evaluate(obj)).toBe(true);
+        expect(assertion.evaluate(obj, assertion.value)).toBe(true);
 
         assertion = new Assertion(undefined, "ignoreCase", "=~", "/TEST2/g");
-        expect(assertion.evaluate(obj)).toBe(true);
+        expect(assertion.evaluate(obj, assertion.value)).toBe(true);
 
         assertion = new Assertion(undefined, "ignoreCase", "=~", "/.*/g");
-        expect(assertion.evaluate(obj)).toBe(true);
+        expect(assertion.evaluate(obj, assertion.value)).toBe(true);
 
         assertion = new Assertion(undefined, "ignoreCase", "=~", "/ABC/g");
-        expect(assertion.evaluate(obj)).toBe(false);
+        expect(assertion.evaluate(obj, assertion.value)).toBe(false);
     });
 
     test("invalid operator", () => {
         const obj = new MockResponse({ val: "Here is a test" });
         let assertion = new Assertion(undefined, "val", "=!", "Here is a test");
         try {
-            assertion.evaluate(obj);
+            assertion.evaluate(obj, assertion.value);
             expect(true).toBe(false);
         } catch (error) {
             expect(true).toBe(true);
