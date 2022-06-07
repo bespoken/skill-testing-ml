7c2240a26387c610fff9eef250359951e7abde1c
Maximo Rosales Diaz <max@bespoken.io>
2022-06-07 13:15:55 -0500

    Added version for new release

--- publishing-toolkit.js
+++ publishing-toolkit.js
@@ -8,15 +8,17 @@ const parseVersion = (version) => {
   const patch = parseInt(patchParts[0])
   const rc = patchParts.length > 1 ? parseInt(patchParts[1]) : null
 
-  const release = `${major}.${minor}.${patch}`
+  const release = `${major}.${minor}.${Math.max(patch, 0)}`
+  const newRelease = `${major}.${minor}.${Math.max(patch, 0) + 1}`
   const candidate = `${major}.${minor}.${patch}-RC.${Math.max(rc, 0) + 1}`
-  return { release, candidate, currentVersion: version }
+  return { release, newRelease, candidate, currentVersion: version }
 }
 
 const printNextRcVersion = () => console.log(readPackage().candidate)
-const printReleaseVersion = () => console.log(readPackage().candidate)
+const printNextReleaseVersion = () => console.log(readPackage().newRelease)
+const printReleaseVersion = () => console.log(readPackage().release)
 const printCurrentVersion = () => console.log(readPackage().currentVersion)
 
 
 
-module.exports = { parseVersion, readPackage, printNextRcVersion, printReleaseVersion, printCurrentVersion }
+module.exports = { parseVersion, readPackage, printNextRcVersion, printReleaseVersion, printCurrentVersion, printNextReleaseVersion }
